bc9a1e8cc60c61f414c1b80e0c9f42000f5ae238
zhangkun999 <66822290+zhangkun999@users.noreply.github.com>
2021-05-25 21:15:11 +0800

    Add files via upload

---
+++ decorator/Changer.java
@@ -0,0 +1,16 @@
+package decorator;
+
+public class Changer implements Transform
+{
+	private Transform transform;
+	
+	public Changer(Transform transform)
+	{
+		this.transform=transform;
+	}
+	
+	public void move()
+	{
+		transform.move();	
+	}
+}
---
+++ decorator/Transform.java
@@ -0,0 +1,6 @@
+package decorator;
+
+public interface Transform
+{
+	public void move();
+}
