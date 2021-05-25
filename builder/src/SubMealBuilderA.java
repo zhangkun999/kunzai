32da52f13154605acfd214f5424a46761e1bf148
zhangkun999 <66822290+zhangkun999@users.noreply.github.com>
2021-05-25 20:27:34 +0800

    Add files via upload
    建造者模式

---
+++ builder/src/SubMealBuilderA.java
@@ -0,0 +1,13 @@
+package builder;
+
+public class SubMealBuilderA extends MealBuilder
+{
+	public void buildFood()
+	{
+		meal.setFood("一个鸡腿堡");
+	}
+	public void buildDrink()
+	{
+	    meal.setDrink("一杯可乐");
+	}
+}
