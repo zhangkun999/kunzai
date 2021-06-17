c1635ee736396734210d9f3ffb6b3d65e02cc607
zhangkun999 <66822290+zhangkun999@users.noreply.github.com>
2021-06-17 18:53:18 +0800

    工厂方法模式

---
+++ factorymethod/src/HaierTV.java
@@ -0,0 +1,9 @@
+package factorymethod;
+
+public class HaierTV implements TV
+{
+	public void play()
+	{
+		System.out.println("海尔电视机播放中......");
+	}
+}
---
+++ factorymethod/src/TVFactory.java
@@ -0,0 +1,6 @@
+package factorymethod;
+
+public interface TVFactory
+{
+    public TV produceTV();
+}
