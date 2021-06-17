0c2f5afda48403f7e2dfd3f66ed4e13e4ada1b38
zhangkun999 <66822290+zhangkun999@users.noreply.github.com>
2021-06-17 19:06:36 +0800

    备忘录模式

---
+++ memento/test/Caretaker.java
@@ -0,0 +1,14 @@
+package memento.test;
+
+public class Caretaker
+{
+	private Memento memento;
+	public Memento getMemento()
+	{
+		return memento;
+	}
+	public void setMemento(Memento memento)
+	{
+		this.memento=memento;
+	}
+}
