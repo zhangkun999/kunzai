b72eed1c4ec140001706abe2ac260604e98471df
zhangkun999 <66822290+zhangkun999@users.noreply.github.com>
2021-06-17 19:03:32 +0800

    解释器模式

---
+++ interpreter/Node.java
@@ -0,0 +1,6 @@
+package interpreter;
+
+public interface Node
+{
+	public int interpret();
+}
---
+++ interpreter/ValueNode.java
@@ -0,0 +1,16 @@
+package interpreter;
+
+public class ValueNode implements Node
+{
+	private int value;
+	
+	public ValueNode(int value)
+	{
+		this.value=value;
+	}
+		
+	public int interpret()
+	{
+		return this.value;
+	}
+}
