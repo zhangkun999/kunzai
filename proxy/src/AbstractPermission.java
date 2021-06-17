87b6e24c0c3252d5b29c099d5f1106ef41097bc1
zhangkun999 <66822290+zhangkun999@users.noreply.github.com>
2021-06-17 18:58:26 +0800

    代理模式

---
+++ proxy/src/AbstractPermission.java
@@ -0,0 +1,10 @@
+package proxy;
+
+public interface AbstractPermission
+{
+	public void modifyUserInfo();
+	public void viewNote();
+	public void publishNote();
+	public void modifyNote();
+	public void setLevel(int level);
+}
