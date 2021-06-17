27c519b70b9ff9ed2f34d3e20d7f3baafc9a3ed8
zhangkun999 <66822290+zhangkun999@users.noreply.github.com>
2021-06-17 18:55:37 +0800

    外观模式

---
+++ facade/AirConditioner.java
@@ -0,0 +1,14 @@
+package facade;
+
+public class AirConditioner
+{
+	public void on()
+	{
+		System.out.println("空调打开！");
+	}
+	
+	public void off()
+	{
+		System.out.println("空调关闭！");
+	}
+}
---
+++ facade/Light.java
@@ -0,0 +1,21 @@
+package facade;
+
+public class Light
+{
+	private String position;
+	
+	public Light(String position)
+	{
+		this.position=position;	
+	}
+	
+	public void on()
+	{
+		System.out.println(this.position + "µÆ´ò¿ª£¡");
+	}
+	
+	public void off()
+	{
+		System.out.println(this.position + "µÆ¹Ø±Õ£¡");	
+	}
+}
